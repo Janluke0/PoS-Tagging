d72b1a92f52df6fd71ea09f715a20e96f97185b7
Janluke <gianlucacaronte@gmail.com>
2022-01-31 18:47:00 +0100

    fix modules

---
+++ dataset/__init__.py
@@ -0,0 +1,1 @@
+from .twtita import TWITADS
---
+++ model/transformers/italian.py
@@ -0,0 +1,50 @@
+from torch import nn
+import torch.nn.functional as F
+
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+
+
+class ItBERTCasedPos(nn.Module):
+    """
+        https://huggingface.co/dbmdz/bert-base-italian-cased
+    """
+
+    def __init__(self, nlabels):
+        super(ItBERTCasedPos, self).__init__()
+        self.nlabels = nlabels
+        self.pretrained = AutoModelForTokenClassification.from_pretrained(
+            "dbmdz/bert-base-italian-cased")
+        self.pretrained.classifier = nn.Linear(
+            self.pretrained.classifier.in_features, self.nlabels)
+
+    def forward(self, *args, **kwargs):
+        scores = self.pretrained(*args, **kwargs)
+        scores = scores.logits
+        return F.log_softmax(scores, dim=1)
+
+    @staticmethod
+    def tokenizer():
+        return AutoTokenizer.from_pretrained("dbmdz/bert-base-italian-cased")
+
+
+class ItBERTUncasedPos(nn.Module):
+    """
+        https://huggingface.co/dbmdz/bert-base-italian-uncased
+    """
+
+    def __init__(self, nlabels):
+        super(ItBERTCasedPos, self).__init__()
+        self.nlabels = nlabels
+        self.pretrained = AutoModelForTokenClassification.from_pretrained(
+            "dbmdz/bert-base-italian-uncased")
+        self.pretrained.classifier = nn.Linear(
+            self.pretrained.classifier.in_features, self.nlabels)
+
+    def forward(self, *args, **kwargs):
+        scores = self.pretrained(*args, **kwargs)
+        scores = scores.logits
+        return F.log_softmax(scores, dim=1)
+
+    @staticmethod
+    def tokenizer():
+        return AutoTokenizer.from_pretrained("dbmdz/bert-base-italian-uncased")
