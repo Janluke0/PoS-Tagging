084a6be7a7026944809be45d86a11eb1b2838387
Janluke <gianlucacaronte@gmail.com>
2022-02-23 16:30:31 +0100

    update pruning nb script

--- notebooks/prune_nb.py
+++ notebooks/prune_nb.py
@@ -1,13 +1,9 @@
 import sys, json, os
 from pathlib import Path
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        _dir = Path(sys.argv[1])
-    else:
-        _dir = Path(os.path.realpath(__file__)).parent
+
+def prune_in_dir(_dir, recursive=True):
     for f in _dir.iterdir():
-        print(f)
         try:
             if f.suffix == '.ipynb':
                 with f.open() as nb:
@@ -15,5 +11,15 @@ if __name__ == '__main__':
                 parsed['metadata']['widgets'] = {}
                 with f.open('w') as nb:
                     nb.write(json.dumps(parsed))
+            elif f.is_dir() and recursive:
+                prune_in_dir(f)
         except:
-            print('err')
+            print('ERROR')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        _dir = Path(sys.argv[1])
+    else:
+        _dir = Path(os.path.realpath(__file__)).parent
+    prune_in_dir(_dir)
