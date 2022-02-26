cdd283f1430c91460b9843e3b2fc95afeedb0d84
Janluke <gianlucacaronte@gmail.com>
2022-02-26 17:36:37 +0100

    add bpemb tokenizer

--- dataset/tokenizer.py
+++ dataset/tokenizer.py
@@ -8,7 +8,11 @@ class UnsupportedTokenizer(Exception):
     pass
 
 
-def get_tokenizer(split_name, _type="BPE", vocab_size=2048, directory="twitads/tokenizers"):
+def get_tokenizer(split_name,
+                  _type="BPE",
+                  vocab_size=2048,
+                  directory="twitads/tokenizers",
+                  **kwargs):
 
     # skip saving and load 4now
     if False:  # not file.exists():
@@ -18,21 +22,23 @@ def get_tokenizer(split_name, _type="BPE", vocab_size=2048, directory="twitads/t
         file.mkdir(parents=True, exist_ok=True)
         #tknzr.save_model(str(file))
 
-    tknzr = train_tokenizer_from_twita(
-        split_name, _type, vocab_size=vocab_size)
+    tknzr = train_tokenizer_from_twita(split_name,
+                                       _type,
+                                       vocab_size=vocab_size,
+                                       **kwargs)
     return tknzr
 
 
-def train_tokenizer_from_twita(split_name, _type="BPE", **kwargs):
+def train_tokenizer_from_twita(split_name, _type="BPE",vocab_size=2048, **kwargs):
     _, tweets = get_ds(split_name)
     sentences = []
     for t in tweets:
         sentences.append(
             " ".join([word for word, _ in t]))
     if _type == "BPE":
-        return train_bpe_from_iter(sentences, **kwargs)
+        return train_bpe_from_iter(sentences, vocab_size, **kwargs)
     elif _type == "WordPiece":
-        return train_wordpiece_from_iter(sentences, **kwargs)
+        return train_wordpiece_from_iter(sentences, vocab_size, **kwargs)
     elif _type == "BERT_pretrained":
         return AutoTokenizer.from_pretrained("dbmdz/bert-base-italian-uncased")
     elif _type == "ELECTRA_pretrained":
@@ -41,6 +47,8 @@ def train_tokenizer_from_twita(split_name, _type="BPE", **kwargs):
         return AutoTokenizer.from_pretrained("Davlan/xlm-roberta-large-ner-hrl")
     elif _type == "DBERT_pretrained":
         return AutoTokenizer.from_pretrained("Davlan/distilbert-base-multilingual-cased-ner-hrl")
+    elif _type == 'BPEmb_pretrained':
+        return BPEmbWrapper(**kwargs)
     else:
         raise UnsupportedTokenizer()
 
@@ -79,3 +87,30 @@ def train_wordpiece_from_iter(sentences,
         ]
     )
     return my_tokenizer
+
+
+from bpemb import BPEmb
+from collections import namedtuple
+
+TokenizedSentence = namedtuple("TokenizedSentence", ['word_ids', 'ids'])
+
+
+class BPEmbWrapper:
+
+    def __init__(self, *args, **kwargs):
+        self.bpemb = BPEmb(*args, **kwargs)
+
+    def encode(self, words, is_pretokenized=True):
+        word_ids, ids = [self.bpemb.BOS], [None]
+        for i, w in enumerate(words):
+            tmp = self.bpemb.encode_ids(w)
+            ids.extend([i] * len(tmp))
+            word_ids.extend(tmp)
+
+        word_ids.append(self.bpemb.EOS)
+        ids.append(None)
+
+        return TokenizedSentence(word_ids, ids)
+
+    def __getattr__(self, name):
+        return getattr(self.bpemb, name)
